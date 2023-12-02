ba12b8920fba192b29813fb7d142cbd6f8e61c59
nova <novaex@tuta.io>
2023-12-02 13:45:08 -0600

    add scraper instance. speed update

--- deg3n.py
+++ deg3n.py
@@ -4,15 +4,21 @@ from discord import app_commands
 import os
 from dotenv import load_dotenv
 import grabber
+import deg3n_scraper
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix="#",intents=discord.Intents.all())
+scraper = deg3n_scraper.deg3n_scraper()
 
 @bot.event
 async def on_ready():
     print(f"User: {bot.user} (ID: {bot.user.id})")
+    if scraper is not None:
+        await scraper.browser_setup()
+        await scraper.page_setup()
+        print("scraper is ready for use...")
 
 @bot.event
 async def on_message(message):
@@ -20,8 +26,8 @@ async def on_message(message):
     if message.author.id == bot.user.id:
         return
     if any(link in message.content for link in ["/twitter.com/","/x.com/"]):
-        embed_url = await grabber.twimg_parse(message.content)
-        await message.reply(embed_url)
+        embed_url = await scraper.twimg_parse(message.content)
+        await message.reply(embed_url,mention_author=False)
 
 @bot.command()
 async def sync(ctx):
